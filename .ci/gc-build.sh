e768da8ce55228f42a168da1eb3f4a92bb21a35b
v.kukins <v.kukins@gmail.com>
2025-09-10 00:20:50 +0300

    version

--- .ci/gc-build.sh
+++ .ci/gc-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+GCLOUD_NPM_REPO=//us-central1-npm.pkg.dev/bubuild/s02-bubuild/
+
 echo Adding authentication information to .npmrc
 echo "registry=${GCLOUD_NPM_REPO}" > .npmrc
 echo "${GCLOUD_NPM_REPO#https:}/:_authToken=$(gcloud auth print-access-token)" >> .npmrc
