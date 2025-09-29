f99aad423dcc91b358b11a28122b0b085a4f2fe3
vku <v.kukins@gmail.com>
2025-09-29 16:03:01 +0300

    ci

--- .ci/setup-npm-repo.sh
+++ .ci/setup-npm-repo.sh
@@ -2,10 +2,6 @@
 
 GCLOUD_NPM_REPO=https://us-central1-npm.pkg.dev/bubuild/s02-bubuild
 
-set -e
-
-cd `dirname $0`/..
-
 echo Adding authentication information to .npmrc
 echo "registry=${GCLOUD_NPM_REPO}/" > .npmrc
 echo "${GCLOUD_NPM_REPO#https:}/:_authToken=$(gcloud auth print-access-token)" >> .npmrc
